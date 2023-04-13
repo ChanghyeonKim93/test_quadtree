45e310e2aa4a5a15ec089a318c8a7528338f36a5
ChanghyeonKim93 <hyun91015@gmail.com>
2023-04-13 21:38:59 +0900

    delete legacy matlab code

--- 20200219_stereoEdgeVO/project3to2.m
+++
@@ -1,6 +0,0 @@
-function pts = project3to2(X,K)
-
-pts = [K(1,1)*X(1,:)./X(3,:) + K(1,3);...
-   K(2,2)*X(2,:)./X(3,:) + K(2,3)];
-
-end
--- 20200219_stereoEdgeVO/warpPts.m
+++
@@ -1,9 +0,0 @@
-function pts_warp = warpPts(pts, depth, g_temp)
-global data_info
-K    = data_info.rectify.K;
-Kinv = data_info.rectify.Kinv;
-
-X_temp = g_temp(1:3,1:3)*Kinv*[pts(1,:); pts(2,:); ones(1,size(pts,2))].*depth + g_temp(1:3,4);
-pts_temp = K*(X_temp./X_temp(3,:));
-pts_warp = pts_temp(1:2,:);
-end
